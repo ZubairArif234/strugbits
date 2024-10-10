6dd625eed043dbc5974943fd310002ca7d6bc96d
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2024-10-10 13:09:34 +0500

    fine details

--- src/stores/meal.store.js
+++ src/stores/meal.store.js
@@ -3,65 +3,58 @@ import { devtools } from "zustand/middleware";
 import custAxios from "../configs/axiosConfig";
 import { successMessage } from "../services/helpers";
 
-// ! this is just an example of how create stores with zustand
 const useMeal = (set) => ({
   allMeals: [],
-  week1:[],
-  week2:[],
-  week3:[],
-  week4:[],
-  loading:false,
-  getAll:async () => {
+  week1: [],
+  week2: [],
+  week3: [],
+  week4: [],
+  loading: false,
+  getAll: async () => {
     try {
-        set({
-          loading: true,
-        });
-        const res = await custAxios.get(`/recipes`);
-        // console.log(res);
-        if (res?.status === 200) {
-         
-          set({
-            loading: false,
-            allMeals:res?.data?.recipes
-          
-          });
-  
-          return true;
-        }
-      } catch (error) {
+      set({ loading: true });
+      const res = await custAxios.get(`/recipes`);
+
+      if (res?.status === 200) {
         set({
           loading: false,
+          allMeals: res?.data?.recipes,
         });
-  
-        // errorMessage(error?.response?.data?.message);
+        return true;
       }
+    } catch (error) {
+      set({ loading: false });
+    }
   },
- addMeal:(meals,week )=> {
-    
-  set((store) => {
-    const existingMeals = store[week] || [];
-  
-    const nonDuplicateMeals = meals?.filter(
-      newMeal => !existingMeals.some(existingMeal => existingMeal.id === newMeal.id)
-    );
-  
-    return {
-      [week]: [...existingMeals, ...nonDuplicateMeals]
-    };
-  });
 
-  successMessage("Meals added successfully.")
-  
- },
- deleteMeal:(meals,week )=> {
-    
-  set( ({
-      [week]:meals
-  }));
+  addMeal: (meals, week) => {
+    set((store) => {
+      const existingMeals = store[week] || [];
 
-  successMessage("Meal removed successfully.")
-  
- }
+      const nonDuplicateMeals = meals?.filter(
+        (newMeal) =>
+          !existingMeals.some((existingMeal) => existingMeal.id === newMeal.id)
+      );
+
+      if (nonDuplicateMeals.length === 0) {
+        successMessage(`All selected meals are already added.`);
+        return {};
+      } else {
+        successMessage("Meals added successfully.");
+        return {
+          [week]: [...existingMeals, ...nonDuplicateMeals],
+        };
+      }
+    });
+  },
+
+  deleteMeal: (meals, week) => {
+    set({
+      [week]: meals,
+    });
+
+    successMessage("Meal removed successfully.");
+  },
 });
 
 export const useMealStore = create(

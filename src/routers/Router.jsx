5ada40c93bb1ac22e953e4c984aa6bd531a221cc
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2024-10-10 13:18:25 +0500

    removed import from router

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout";
 import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
 
 const Router = () => {
   // use protected routes for authenticated users (i.e: UserRoute & AdminRoute or make more if you've to)..
@@ -14,11 +13,6 @@ const Router = () => {
         <Route path="/" element={<Home />} />
       </Route>
 
-      {/* Auth routes */}
-      
-
-      {/* Not found page */}
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
